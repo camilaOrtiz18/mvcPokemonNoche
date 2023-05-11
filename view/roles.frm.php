db41689081c4d714685a7ce3e810ce97acc21792
Camila C <camcasor@gmail.com>
2023-05-10 19:11:34 -0500

    Primer commit MVC

---
+++ view/roles.frm.php
@@ -0,0 +1,28 @@
+<?php include_once "header.php"; ?>
+
+<div class="row my-4">
+    <h1 class="text-center">Formulario roles</h1>
+</div>
+<div class="row">
+    <div class="col-3 d-flex justify-content-end align-items-center">
+        <span class="fw-bolder">Nombre rol</span>
+    </div>
+
+    <div class="col-7">
+        <div class="form-floating">
+            <input type="text" class="form-control" id="floatingInput" placeholder="admin">
+            <label for="floatingInput">Nombre Rol</label>
+        </div>
+    </div>
+
+</div>
+<div class="row bg-dark mt-5">
+    <h1 class="text-light text-center">Tabla de roles</h1>
+</div>
+
+
+
+
+
+
+<?php include_once "footer.php"; ?>
